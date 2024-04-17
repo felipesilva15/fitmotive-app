719cd9cf2aaf2a93afaed06532d4110991ef7277
FELIPE DA SILVA SANTOS <felipe.ssantos80@senacsp.edu.br>
2024-04-16 21:37:59 -0300

    refactor: Adicionando o tipo de retorno dos métodos do service de usuários

--- src/app/main/service/user.service.ts
+++ src/app/main/service/user.service.ts
@@ -1,7 +1,7 @@
 import { User } from './../api/user';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -12,14 +12,14 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUser (id: number) {
+  getUser (id: number): Observable<User> {
     return this.http.get<User>(`${this.baseUrl}/${id}`)
       .pipe(
         tap(console.log)
       );
   }
 
-  getUsers () {
+  getUsers (): Observable<User[]> {
     return this.http.get<User[]>(`${this.baseUrl}`)
       .pipe(
         tap(console.log)
