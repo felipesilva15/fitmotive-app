e8b687c3e3363d0b52e82b4d7ea5814625e6a909
felipe.silva <felipe.silva@pwi.com.br>
2024-04-17 18:09:22 -0300

    feat: Criando base da página de consulta de pacientes

--- src/app/main/components/patient/list-patient/list-patient.component.ts
+++ src/app/main/components/patient/list-patient/list-patient.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Table } from 'primeng/table';
+import { User } from 'src/app/main/api/user';
 
 @Component({
   selector: 'app-list-patient',
-  standalone: true,
-  imports: [],
   templateUrl: './list-patient.component.html',
   styleUrl: './list-patient.component.scss'
 })
 export class ListPatientComponent {
+  patients: User[] = [];
+  selectedPatients: User[] = [];
+  cols: any[] = [];
 
+  constructor() {}
 }
