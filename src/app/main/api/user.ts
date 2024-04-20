a90632623c1062653c312eb9dba6eb941c4054bf
Felipe Silva <felipe.allware@gmail.com>
2024-04-20 16:34:22 -0300

    feat: Adicionando campo de senha na interface de usuário

--- src/app/main/api/user.ts
+++ src/app/main/api/user.ts
@@ -4,15 +4,16 @@ import { Phone } from "./phone";
 
 export interface User {
     id?: number;
-    name: string;
-    email: string;
-    cpf_cnpj: string;
-    birth_date: string | Date;
+    name?: string;
+    email?: string;
+    password?: string;
+    cpf_cnpj?: string;
+    birth_date?: string | Date;
     bank_gateway_id?: string;
-    inactive: boolean;
-    service_price: number;
-    created_at: string | Date;
-    updated_at: string | Date;
+    inactive?: boolean;
+    service_price?: number;
+    created_at?: string | Date;
+    updated_at?: string | Date;
     phones?: Array<Phone>;
     payment_methods?: Array<PaymentMethod>;
     adresses?: Array<Address>;
