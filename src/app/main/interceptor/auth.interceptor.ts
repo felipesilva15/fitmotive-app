6e138564b5d6a57f7814d505ac76126853df5577
FELIPE DA SILVA SANTOS <felipe.ssantos80@senacsp.edu.br>
2024-04-23 21:35:17 -0300

    feat: Chamando modal de erros quando ocorre erro na API

--- src/app/main/interceptor/auth.interceptor.ts
+++ src/app/main/interceptor/auth.interceptor.ts
@@ -10,12 +10,14 @@ import {
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { ErrorDialogService } from '../service/error-dialog.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(
     private authService: AuthService,
-    private router: Router) {
+    private router: Router,
+    private errorDialogService: ErrorDialogService) {
 
   }
 
@@ -36,14 +38,14 @@ export class AuthInterceptor implements HttpInterceptor {
           console.error(err);
 
           if (err instanceof HttpErrorResponse) {
+            let message = err.error.message ? err.error.message : 'Erro inesperado. Tente novamente mais tarde ou entre em contato com o administrador do sistema.';
+
+            this.errorDialogService.showDialogError(message);
+
             if (err.status == 401) {
               this.router.navigate(['/auth/login']);
               return;
-            } 
-            
-            let message = err.error.message ? err.error.message : 'Erro inesperado. Tente novamente mais tarde ou entre em contato com o administrador do sistema.';
-
-            console.log(message);
+            }
           }
         }
       )
