278541cd11637f83bad853f16c462a30260b5d77
Felipe Silva <felipe.allware@gmail.com>
2024-05-01 12:39:34 -0300

    feat: Ajustando model de provider

--- src/app/main/api/provider.ts
+++ src/app/main/api/provider.ts
@@ -1,3 +1,19 @@
+import { Address } from "./address";
+import { PaymentMethod } from "./payment-method";
+import { Phone } from "./phone";
+
 export interface Provider {
     id?: number;
+    user_id?: number;
+    name: string;
+    email: string;
+    cpf_cnpj: string;
+    birth_date: string | Date;
+    bank_gateway_id?: string;
+    inactive: boolean;
+    phones?: Array<Phone>;
+    payment_methods?: Array<PaymentMethod>;
+    adresses?: Array<Address>;
+    created_at?: string | Date;
+    updated_at?: string | Date;
 }
