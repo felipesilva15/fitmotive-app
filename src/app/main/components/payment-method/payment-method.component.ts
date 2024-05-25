a921b9490570ddee4eccb41a62d72ea4af2fe267
Felipe Silva <felipe.allware@gmail.com>
2024-05-25 01:59:02 -0300

    feat: Adicionando load de sinc. de status de cobrança e correção de modal de metodo de pagto.

--- src/app/main/components/payment-method/payment-method.component.ts
+++ src/app/main/components/payment-method/payment-method.component.ts
@@ -33,6 +33,7 @@ export class PaymentMethodComponent {
 
     this.formGroup = this.buildFormGroup();
     this.checkType();
+    
   }
 
   private buildFormGroup(): FormGroup {
@@ -41,7 +42,7 @@ export class PaymentMethodComponent {
       card_number: [{value: this.data.card_number, disabled: true}, []],
       network_token: [{value: this.data.network_token, disabled: true}, []],
       exp_date: [{value: this.data.exp_month + this.data.exp_year, disabled: true}, []],
-      security_code: [{value: this.data.security_code, disabled: true}, []],
+      security_code: [{value: this.data.security_code, disabled: true}, [Validators.maxLength(3)]],
       main: [this.data.main, []]
     })
   }
@@ -80,7 +81,7 @@ export class PaymentMethodComponent {
 
       this.card_number.setValidators([Validators.required]);
       this.exp_date.setValidators([Validators.required]);
-      this.security_code.setValidators([Validators.required]);
+      this.security_code.setValidators([Validators.required, Validators.maxLength(3)]);
     } else {
       this.isCard = false;
       
@@ -96,7 +97,7 @@ export class PaymentMethodComponent {
 
       this.card_number.removeValidators([Validators.required]);
       this.exp_date.removeValidators([Validators.required]);
-      this.security_code.removeValidators([Validators.required]);
+      this.security_code.removeValidators([Validators.required, Validators.maxLength(3)]);
     }
   }
   
@@ -116,11 +117,12 @@ export class PaymentMethodComponent {
   }
 
   submit(): void {
-    if (!this.formGroup.valid) {
-      return;
-    }
+    console.log(this.security_code)
+    // if (!this.formGroup.valid) {
+    //   return;
+    // }
 
-    this.convertFormToObject()
-    this.close(this.data)
+    // this.convertFormToObject()
+    // this.close(this.data)
   }
 }
